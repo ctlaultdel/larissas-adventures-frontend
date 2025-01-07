022b083e9f6cf7352151ce521685fbb0025c3810
larissa <larza.ault@gmail.com>
2025-01-06 19:44:06 -0800

    added aboutme and instagram link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import headshot from "./imgs/headshot.png";
 import logo from "./imgs/logo.png";
-import aboutme from "./aboutme.txt";
+import instagramlogo from "./imgs/instagramlogo.png";
 import "./App.css";
+
 // placecards for adventure cards ~ temporary
 import athabasca from "./imgs/adventure_cards/athabasca.jpg";
 import lepetitcheval from "./imgs/adventure_cards/lepetitcheval.jpg";
@@ -9,6 +10,15 @@ import shuksan from "./imgs/adventure_cards/shuksan.jpg";
 import Greece from "./imgs/adventure_cards/Greece.jpg";
 import bozeman from "./imgs/adventure_cards/bozeman.jpg";
 import flyboys from "./imgs/adventure_cards/flyboys.jpg";
+import iceland from "./imgs/adventure_cards/iceland.jpg";
+import oahu from "./imgs/adventure_cards/oahu.jpg";
+import westcoast from "./imgs/adventure_cards/westcoast.jpg";
+import kauai from "./imgs/adventure_cards/kauai.jpg";
+import france from "./imgs/adventure_cards/france.jpg";
+import maui from "./imgs/adventure_cards/maui.jpeg";
+import cairns from "./imgs/adventure_cards/cairns.jpg";
+import crosscountry from "./imgs/adventure_cards/crosscountry.jpeg";
+
 //
 
 function App() {
@@ -19,6 +29,14 @@ function App() {
     { name: "Flyboys", img: flyboys },
     { name: "Bozeman", img: bozeman },
     { name: "Greece", img: Greece },
+    { name: "Iceland", img: iceland },
+    { name: "Oregon California Coast", img: westcoast },
+    { name: "Oahu", img: oahu },
+    { name: "Kauai", img: kauai },
+    { name: "France", img: france },
+    { name: "Cairns", img: cairns },
+    { name: "Maui", img: maui },
+    { name: "Cross Country USA - North", img: crosscountry },
   ];
 
   return (
@@ -30,7 +48,21 @@ function App() {
       <div className="intro-container">
         <div className="about-me-section">
           <div className="title">Bonjour, je suis Larissa!</div>
-          <embed className="about-me" src={aboutme}></embed>
+          <p className="about-me">
+            I am a traveler, adventurist, and lifelong learner. My goal is to
+            experience the world by learning about other cultures through
+            travel, embracing challenge and uncertainty through adventures, and
+            live my life to the fullest! I hope my stories inspire you and share
+            beta for your own adventures 🖤
+          </p>
+          <a href="https://www.instagram.com/lollapalarza/?hl=en">
+            <img
+              alt="instagram"
+              src={instagramlogo}
+              className="instagram-logo"
+            ></img>
+            <p className="handle">lollapalarza</p>
+          </a>
         </div>
         <div className="headshot">
           <img src={headshot} alt="headshot" />
@@ -46,9 +78,10 @@ function App() {
                   key={thumbnail.name.replaceAll(" ", "_")}
                   src={thumbnail.img}
                   alt={thumbnail.name}
-                  className="adventure-thumbnails"
+                  className="thumbnails"
                 ></img>
               </a>
+              <div className="name">{thumbnail.name}</div>
             </div>
           ))}
         </div>
